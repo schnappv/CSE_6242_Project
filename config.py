595b0697800be868a92422d8217100b7de740b38
Valerie Schnapp <valerie.f.schnapp@gmail.com>
2021-10-07 15:37:06 -0400

    updated configuration to connect to mysql

--- config.py
+++ config.py
@@ -4,12 +4,14 @@ from mysql.connector.constants import ClientFlag
 config = {
     'user': 'root',
     'password': 'avatar',
-    'host': '94.944.94.94',
+    'host': '34.85.166.17',
     'client_flags': [ClientFlag.SSL],
     'ssl_ca': 'ssl_keys/server-ca.pem',
     'ssl_cert': 'ssl_keys/client-cert.pem',
     'ssl_key': 'ssl_keys/client-key.pem'
 }
 
-# now we establish our connection
-cnxn = mysql.connector.connect(**config)
+
+def connect():
+    cnxn = mysql.connector.connect(**config)
+    return cnxn
