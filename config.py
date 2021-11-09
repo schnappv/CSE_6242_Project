60f33d2c5ee9c6240253deaced21c7f47b931edc
Valerie Schnapp <valerie.f.schnapp@gmail.com>
2021-11-08 19:53:28 -0500

    updated configurations

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import mysql.connector
 from mysql.connector.constants import ClientFlag
 
 config = {
-    'user': 'root',
-    'password': 'avatar',
-    'host': '34.85.166.17',
-    'client_flags': [ClientFlag.SSL],
-    'ssl_ca': 'ssl_keys/server-ca.pem',
-    'ssl_cert': 'ssl_keys/client-cert.pem',
-    'ssl_key': 'ssl_keys/client-key.pem'
+    "user": "root",
+    "password": "avatar",
+    "host": "35.188.250.217",
+    "client_flags": [ClientFlag.SSL],
+    "ssl_ca": "ssl_keys/server-ca.pem",
+    "ssl_cert": "ssl_keys/client-cert.pem",
+    "ssl_key": "ssl_keys/client-key.pem",
 }
 
 
